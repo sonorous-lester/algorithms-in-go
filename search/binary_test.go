51d80eab09ebabefaabc57fe44b36f1b342795ec
Lester <sonorous.lester@gmail.com>
2023-04-24 07:41:55 +0800

    Implement binary search

--- search/binary_test.go
+++ search/binary_test.go
@@ -20,19 +20,41 @@ func TestBinarySearch(t *testing.T) {
 			result:           true,
 		},
 		{
-			input:            []int{1, 2, 3, 4, 6, 9, 12, 34, 68, 89},
+			input:            []int{1, 2, 3, 4, 6, 9, 12, 34, 68},
 			numberWantToFind: 20,
 			result:           false,
 		},
 	}
 
 	for _, ts := range cases {
-		find := binarySearch(ts.input)
+		find := binarySearch(ts.numberWantToFind, ts.input)
 		assert.Equal(t, ts.result, find)
 	}
 
 }
 
-func binarySearch(numbers []int) bool {
-	return false
+func binarySearch(want int, numbers []int) bool {
+	//	find the middle number in the array(actual)
+	//	if the actual is equal to want return true
+	//	if the actual is latest and is not equal to want return false
+	//	if the actual is bigger then want
+	//		search right side of the array
+	//	if the actual is smaller than want
+	//		search left side of the array
+
+	middle := len(numbers) / 2
+	actual := numbers[middle]
+	if actual == want {
+		return true
+	}
+
+	if middle == 0 && actual != want {
+		return false
+	}
+
+	if actual > want {
+		return binarySearch(want, numbers[:middle])
+	} else {
+		return binarySearch(want, numbers[middle:])
+	}
 }
