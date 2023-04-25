1a31b29bd6d089f3c1903686e3a4086affec830d
Lester <sonorous.lester@gmail.com>
2023-04-25 22:17:30 +0800

    Add bubble sort scaffolding

---
+++ sort/bubblesort_test.go
@@ -0,0 +1,33 @@
+package sort
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestBubbleSort(t *testing.T) {
+	tss := []testcase{
+		{
+			in:     []int{9, 8, 5, 7, 3, 2, 1, 4, 6, 0},
+			expect: []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
+		},
+		{
+			in:     []int{2, 1},
+			expect: []int{1, 2},
+		},
+		{
+			in:     []int{0},
+			expect: []int{0},
+		},
+	}
+
+	for _, ts := range tss {
+		actual := bubbleSort(ts.in)
+		assert.ElementsMatch(t, ts.expect, actual)
+	}
+
+}
+
+func bubbleSort(input []int) []int {
+	return nil
+}
