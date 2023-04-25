69f3236009bee4d3018184f0109b3464799b0b24
Lester <sonorous.lester@gmail.com>
2023-04-25 21:28:02 +0800

    Create selection sort scaffolding

---
+++ sort/selectsort_test.go
@@ -0,0 +1,38 @@
+package sort
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+type testcase struct {
+	in     []int
+	expect []int
+}
+
+func TestSelectionSort(t *testing.T) {
+	tss := []testcase{
+		{
+			in:     []int{9, 8, 5, 7, 3, 2, 1, 4, 6, 0},
+			expect: []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
+		},
+		{
+			in:     []int{2, 1},
+			expect: []int{1, 2},
+		},
+		{
+			in:     []int{0},
+			expect: []int{0},
+		},
+	}
+
+	for _, ts := range tss {
+		actual := selectionSort(ts.in)
+		assert.ElementsMatch(t, ts.expect, actual)
+	}
+
+}
+
+func selectionSort(nums []int) []int {
+	return nil
+}
