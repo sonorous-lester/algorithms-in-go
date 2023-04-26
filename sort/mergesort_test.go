056a01d82fcacea68fc5bb500581e8340a92d471
Lester <sonorous.lester@gmail.com>
2023-04-26 21:28:40 +0800

    Implement merge sort

--- sort/mergesort_test.go
+++ sort/mergesort_test.go
@@ -12,8 +12,8 @@ func TestMergeSort(t *testing.T) {
 			expect: []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
 		},
 		{
-			in:     []int{2, 1},
-			expect: []int{1, 2},
+			in:     []int{3, 2, 1},
+			expect: []int{1, 2, 3},
 		},
 		{
 			in:     []int{0},
@@ -29,5 +29,42 @@ func TestMergeSort(t *testing.T) {
 }
 
 func mergeSort(nums []int) []int {
-	return nil
+
+	if len(nums) == 1 {
+		return nums
+	}
+
+	middle := len(nums) / 2
+	left := mergeSort(nums[:middle])
+	right := mergeSort(nums[middle:])
+	return merge(left, right)
+
+}
+
+func merge(left, right []int) []int {
+
+	var out []int
+
+	leftIndex := 0
+	rightIndex := 0
+	for leftIndex < len(left) && rightIndex < len(right) {
+		if left[leftIndex] < right[rightIndex] {
+			out = append(out, left[leftIndex])
+			leftIndex++
+		} else {
+			out = append(out, right[rightIndex])
+			rightIndex++
+		}
+	}
+
+	for ; rightIndex < len(right); rightIndex++ {
+		out = append(out, right[rightIndex])
+	}
+
+	for ; leftIndex < len(left); leftIndex++ {
+		out = append(out, left[leftIndex])
+	}
+
+	return out
+
 }
